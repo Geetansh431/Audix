c7cb95dbd7d698a2e10aa522bf1db6ca2c145361
Geetansh Goyal <geetanshg2@gmail.com>
2026-07-15 02:34:14 +0530

    Refactor App component and restructure assets
    - Removed unnecessary imports and code from App.tsx, integrating a new Home component.
    - Added new font files and images to the assets directory.
    - Deleted unused hero image and SVG files for React and Vite.
    - Created a new Home component with a video background and navigation links.
    - Added corresponding CSS for the Home component to style the layout and elements.
    - Updated index.css to simplify styles and improve font rendering.

---
+++ src/components/Hero/Home.tsx
@@ -0,0 +1,37 @@
+import "./home.css";
+import A from "../../assets/img/bg.mp4";
+
+const Home = () => {
+  return (
+    <div className="h">
+      <div className="h-main">
+        <div className="logo">itsraag</div>
+
+        <nav className="langu">
+          <a href="https://www.instagram.com/itsraag/" className="Button">
+            INSTA
+          </a>
+          <a href="https://www.youtube.com/@itsraag/shorts" className="Button">
+            YT
+          </a>
+        </nav>
+        <div className="heading">
+          We don't do <br />
+          advertising.
+        </div>
+        <div className="view">
+          <a href="#" className="Button-view">
+            View Manifesto
+          </a>
+        </div>
+        <div className="date">2021 — where it began</div>
+
+        <video loop muted autoPlay className="backdrop">
+          <source src={A} type="video/mp4" />
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
